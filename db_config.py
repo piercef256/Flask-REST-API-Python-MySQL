e1b0714cd545294b21fbc2c6037df10eed4f866e
piercef256 <42844463+piercef256@users.noreply.github.com>
2022-01-29 14:52:13 -0800

    env variable test

--- db_config.py
+++ db_config.py
@@ -6,7 +6,7 @@ load_dotenv()
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = "admin"
+app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQL_DATABASE_USER")
 app.config['MYSQL_DATABASE_PASSWORD'] = "adminpassword1"
 app.config['MYSQL_DATABASE_DB'] = "todolist"
 app.config['MYSQL_DATABASE_HOST'] = "database-1.cclou8jjrcwz.us-west-2.rds.amazonaws.com"
